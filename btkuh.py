9800b50ff7173181d5cf310929d2ebce70ec9551
J3Patel <teenenggr@gmail.com>
2020-04-01 17:46:53 +0530

    added some code to read sensor

--- btkuh.py
+++ btkuh.py
@@ -2,50 +2,74 @@ import schedule as s
 import time
 import pytz
 from datetime import datetime as dt
-
-
+from playsound import playsound as ps
+from threading import Thread
+from time import sleep
+import signal
 
 
 tz_IN = pytz.timezone('Asia/Kolkata')
 dt_IN = dt.now(tz_IN)
 
 t = dt_IN.hour
-
+interrupted = False
 state = 0 
-# 0 - "alarm"
 # 1 - "bottle picked"
 # 2 - "bottle placed"
 
+def signal_handler(signal, frame):
+    # example.stop()
+    print("interrupted")
+    global interrupted
+    interrupted = True
+
+def sensorDataRead():
+	print("data read")
+	# time.sleep(5)
+	print("data read complete")
+
+def playAudio(s):
+	if s == "drink":
+		playsound("")
+	elif s == "refill":
+		playsound("")
+	elif s == "place":
+		playsound("")
+
 def drink():
 
-	playSound()
+	playAudio("drink")
 	print("Drink water")
 
 	while state != 1:
 		time.sleep(5)
-		playSound()
-	# time.sleep(150)
+		playAudio("drink")
+	time.sleep(150)
 	while state != 2:
+		time.sleep(5)
+		playAudio("place")
 
 	if bottleEmpty:
 		refillBott()		
 	
 
-
 def refillBott():
 	print("Refill Bottle")
-	playSound()
+	playAudio("refill")
 	while state != 1:
 		time.sleep(1)
-		playSound()
+		playAudio("refill")
+	time.sleep(150)
 	while state != 2:
+		time.sleep(5)
+		playAudio("place")
 
 	if not bottleFull:
 		refillBott()
 
 
 def job():
-	
+	drink()
 	return
 
 def startHourly():
@@ -53,6 +77,15 @@ def startHourly():
 
 def stopHourly():
 	s.clear("hourly")
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# th = Thread(target = sensorDataRead, args = ())
+# th.start()
+
+
+# capture SIGINT signal, e.g., Ctrl+C
 
 # if t > 5:
 # 	s.every().day.at(str(t+1)+":00").do(startHourly)	
@@ -62,7 +95,8 @@ def stopHourly():
 # s.every().day.at("21:00").do(stopHourly)
 
 
-# while True:
-# 	s.run_pending()
-# 	time.sleep(1)
+while not interrupted:
+	sensorDataRead()
+	s.run_pending()
+	time.sleep(1)
 
